a8878ea8116d422d65e416cd4a37000c527f7691
Josh L <codezen@posteo.us>
2018-11-18 20:24:01 -0500

    Misc jBone compatibility fixes

--- app/gallery-tools/grid.js
+++ app/gallery-tools/grid.js
@@ -8,21 +8,21 @@ define(function (require) {
         addFilterToGridWrapper: function (gridWrapper, searchPlaceholder) {
             const filterSlideBasedOnSearchTerm = R.curry(function (filterString, slide) {
                 const $slide = $(slide);
-                if ($('.image-slide-title', $slide).get('innerText').toLowerCase().indexOf(filterString.toLowerCase()) > -1)
-                    $slide.show();
+                if ($('.image-slide-title', $slide).get(0).innerText.toLowerCase().indexOf(filterString.toLowerCase()) > -1)
+                    $slide.removeClass('yui3-widget-hidden');
                 else
-                    $slide.hide();
+                    $slide.addClass('yui3-widget-hidden');
             });
 
             const filterSlidesAfterInactivity = R.curry(function($slidesToFilter, e) {
                 const $filterTextBox = $(e.target);
-                clearTimeout($filterTextBox.get('filterTimer'));
+                clearTimeout($filterTextBox.data('filterTimer'));
 
                 const doFilter = function () {
-                    $slidesToFilter.each(filterSlideBasedOnSearchTerm($filterTextBox.get('value')));
+                    $slidesToFilter.toArray().forEach(filterSlideBasedOnSearchTerm($filterTextBox.val()));
                 };
 
-                $filterTextBox.set('filterTimer', setTimeout(doFilter, 500));
+                $filterTextBox.data('filterTimer', setTimeout(doFilter, 500));
             });
 
             const $gridSlides = $('.slide', gridWrapper);
