989ffc3ed0989492ac66ac2afe02017510900bbb
Josh L <codezen@posteo.us>
2018-11-18 19:31:24 -0500

    removed paths, unneeded as baseURL config of requireJS is set via data-main attribute

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 requirejs.config({
     paths: {
-        // "dwp-ext": "https://cdn.jsdelivr.net/gh/pr0ggy/dwp-ext@6b76c836f0d12604311bee53f00397adbb074405/"
-        "dwp-ext": "https://cdn.jsdelivr.net/gh/pr0ggy/dwp-ext"
+        
     }
 });
